e4f40b9b1c13754c614b730eabd42d9246de75a4
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 19:35:44 +0100

    inicio

---
+++ papa/src/papa/Ja.java
@@ -0,0 +1,20 @@
+package papa;
+
+public class Ja {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");	System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");	System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+	}
+
+}
