53763260c027e470b12454c5498b598ebdbfceae
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 19:41:03 +0100

    mucha paja

--- papa/src/papa/Ja.java
+++ papa/src/papa/Ja.java
@@ -2,11 +2,22 @@ package papa;
 
 public class Ja {
 
+	public boolean visualizarSalida(boolean acertada, int i, String newParam) {
+		return acertada;
+	}
+			
+			
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");	System.out.println("hola");
 		System.out.println("hola");
 		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");
+		System.out.println("hola");	System.out.println("hola");
 		System.out.println("hola");
 		System.out.println("hola");
 		System.out.println("hola");
